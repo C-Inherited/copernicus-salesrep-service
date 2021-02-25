6434087fc1b563b900b49a5a39ef6e7934ad38fc
Nerea García <nereitagp12@gmail.com>
2021-02-25 21:30:06 +0100

    add create table salesrep

---
+++ src/main/resources/sql/salesrep.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `sales_rep` (
+                             `sales_rep_id` int NOT NULL AUTO_INCREMENT,
+                             `name` varchar(255) DEFAULT NULL,
+                             PRIMARY KEY (`sales_rep_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
